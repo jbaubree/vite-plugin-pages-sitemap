c1b7f240d0bda0bf8a3d123fd9aa3b4507f1590a
JB Aubrée <jb.aubree@digitsole.com>
2022-02-13 11:57:16 +0100

    remove vue-router from package.json

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
-import type { RouteRecordRaw } from 'vue-router'
 import generateSitemap from 'vite-plugin-pages-sitemap'
 
 const config = defineConfig({
   plugins: [
     Vue(),
     Pages({
-      onRoutesGenerated: (routes: RouteRecordRaw[]) => (generateSitemap({ routes })),
+      onRoutesGenerated: routes => (generateSitemap({ routes })),
     }),
   ],
 })
